d9c20a7d9e12ed3462eb6a634287623e87c31714
rtw89h7gjsp76fp8902727 <131386537+rtw89h7gjsp76fp8902727@users.noreply.github.com>
2023-05-25 23:15:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-const buttons = document.querySelectorAll(".card-buttons button");
-const sections = document.querySelectorAll(".card-section");
-const card = document.querySelector(".card");
+//got the idea from 2 other pens around here. 
 
-const handleButtonClick = e => {
-  const targetSection = e.target.getAttribute("data-section");
-  const section = document.querySelector(targetSection);
-  targetSection !== "#about" ?
-  card.classList.add("is-active") :
-  card.classList.remove("is-active");
-  card.setAttribute("data-state", targetSection);
-  sections.forEach(s => s.classList.remove("is-active"));
-  buttons.forEach(b => b.classList.remove("is-active"));
-  e.target.classList.add("is-active");
-  section.classList.add("is-active");
-};
-
-buttons.forEach(btn => {
-  btn.addEventListener("click", handleButtonClick);
-});
+// https://codepen.io/codetrail/pen/uHvpl
+// https://codepen.io/kaypooma/pen/ehfjC
